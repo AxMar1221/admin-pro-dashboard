095123637d9e1b1e2c59d46f58449da8ed793468
Tachi Dito <tachidito_7inc@hotmail.com>
2022-12-28 10:14:33 -0600

    fetch promise

--- src/app/pages/promes/promes.component.ts
+++ src/app/pages/promes/promes.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-promes',
@@ -6,6 +6,24 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class PromesComponent {
+export class PromesComponent implements OnInit{
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.getUsers().then( users => {
+      console.log(users);
+    });
+  }
+
+  getUsers(){
+
+    return new Promise( resolve => {
+
+      fetch('https://reqres.in/api/users')
+      .then( resp => resp.json())
+      .then( body => resolve(body.data));
+    });
+  }
 
 }
